9e5a93ccfcbff62a65b66b87d094779b9daba10e
Dmitry Vyal <akamaus@gmail.com>
2014-01-31 19:54:14 +0400

    compiles

--- src/CNC/Declarative.hs
+++ src/CNC/Declarative.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving, ScopedTypeVariables #-}
 module Declarative where
 
 import CNC.Geometry(RealT, eps)
@@ -15,9 +15,11 @@ import Data.Complex
 import Text.Printf
 
 newtype PointInd = PointInd Int deriving (Eq, Ord, Enum, Show)
+
 type Pos = Complex RealT
 
-data Figure = Figure { _fPoints ::  M.Map PointInd (Maybe Pos), _fNextPoint :: PointInd, _fConstraints :: [Constraint] } deriving Show
+type FigMap = M.Map PointInd (Maybe Pos)
+data Figure = Figure { _fPoints ::  FigMap, _fNextPoint :: PointInd, _fConstraints :: [Constraint] } deriving Show
 
 mkPos x y = x :+ y
 
@@ -28,14 +30,22 @@ empty_figure = Figure M.empty zero_ind []
 figNumPoints fig = case _fNextPoint fig of
   PointInd k -> k
 
-instance Ord a => Ord (Complex a) where
+instance (Ord a, RealFloat a) => Ord (Complex a) where
   compare x y = let c1 = compare (realPart x) (realPart y)
                 in if c1 == EQ then compare (imagPart x) (imagPart y)
                    else c1
 
-data Constraint = Disp {cPoint1 :: PointInd, cPoint2 :: PointInd, cPos :: Pos} -- A vector from first point to second
-                | Length {cPoint1 :: PointInd, cPoint2 :: PointInd, cLen :: RealT} -- distance between points
-                | Angle {cPoint1 :: PointInd, cPoint2 :: PointInd, cAngle :: RealT} deriving (Eq, Ord, Show)
+data UniConstraint pt = Disp {cPoint1 :: pt, cPoint2 :: pt, cPos :: Pos} -- A vector from first point to second
+                      | Length {cPoint1 :: pt, cPoint2 :: pt, cLen :: RealT} -- distance between points
+                      | Angle {cPoint1 :: pt, cPoint2 :: pt, cAngle :: RealT} deriving (Eq, Ord, Show)
+
+instance Functor UniConstraint where
+  fmap f c = case c of
+    Disp p1 p2 d -> Disp (f p1) (f p2) d
+    Length p1 p2 l -> Length (f p1) (f p2) l
+    Angle p1 p2 a -> Angle (f p1) (f p2) a
+
+type Constraint = UniConstraint PointInd
 
 mkLabels [''Figure]
 
@@ -59,51 +69,61 @@ solveFigure :: Figure -> [Pos]
 solveFigure fig | figNumPoints fig == 0 = error "can't solve figure"
                 | otherwise = let points = M.insert zero_ind (Just zero_point) $ get fPoints fig
                                   constraints = sort $ get fConstraints fig
-                                  solution = solveConstraints points constraints
-                              in checkSolution solution constraints
+                                  solution = solveConstraints points constraints [] False
+                              in map fromJust $ M.elems $ checkSolution solution constraints
 
-solveConstraints :: M.Map PointInd Pos -> [Constraint] -> [Constraint] -> Bool -> [Pos]
+solveConstraints :: FigMap -> [Constraint] -> [Constraint] -> Bool -> FigMap
 solveConstraints points [] [] _ = points -- no more constraints, finish here
-solveConstraints points [] prev_cs True = solveConstraints points (reverse prev_cs) False -- iterating over constraints again
-solveConstraints points [] prev_cs False = checkSolution points prev_cs -- no constraints was applied, finishing
+solveConstraints points [] prev_cs True = solveConstraints points (reverse prev_cs) [] False -- iterating over constraints again
+solveConstraints points [] prev_cs False = points -- no constraints was applied, finishing
 solveConstraints points (c:cs) prev_cs shrinked = case c of -- process a single constraint
-  Disp pi1 pi2 disp -> case (M.lookup pi1 points, M.lookup pi2 points) of
+  Disp pi1 pi2 disp -> case (points ! pi1, points ! pi2) of
     (Nothing, Nothing) -> skipConstr
     (Just p1, Nothing) -> solveDisp p1 pi2 disp
     (Nothing, Just p2) -> solveDisp p2 pi1 (-disp)
-    (Just p1, Just p2) -> checkConstr (abs $ p2 - p1 - disp)
-  Length pi1 pi2 len -> case (M.lookup pi1 points, M.lookup pi2 points) of
+    (Just p1, Just p2) -> checkConstr
+  Length pi1 pi2 len -> case (points ! pi1, points ! pi2) of
     (Nothing, Nothing) -> skipConstr
-    (Just p1, Nothing) -> solveLength p1 pi2 1 len
-    (Nothing, Just p2) -> solveLength p2 pi1 (-1) len
-    (Just p1, Just p2) -> checkConstr (abs (p1 - p2) - len)
+    (Just pp1, Nothing) -> solveLength pp1 pi1 pi2 1 len
+    (Nothing, Just p2) -> solveLength p2 pi2 pi1 (-1) len
+    (Just p1, Just p2) -> checkConstr
   Angle pi1 pi2 a -> case (M.lookup pi1 points, M.lookup pi2 points) of -- only check these, they're used as part of Length constraints processing
-    (Just p1, Just p2) -> checkConstr (phase (p2 - p1) - a)
+    (Just p1, Just p2) -> checkConstr
     _ -> skipConstr
   where
+    pointConstr = fmap (fromJust . (points !)) c
+    checkConstr = checkEps (calcDiscrepancy pointConstr) (solveConstraints points cs prev_cs shrinked) ("disagreement while processing " ++ show c)
     skipConstr = solveConstraints points cs (c:prev_cs) shrinked
-    checkConstr dist = if dist < eps then solveConstraints points cs prev_cs shrinked else error $ "disagreement while processing " ++ show c
-    solveDisp p ind d = solveConstraints (M.insert pi points, Just $ p + d) cs prev_cs True
-    solveLength p ind ind0 d a_mult = case findAngleConstr ind0 ind of
+    solveDisp p ind d = solveConstraints (M.insert ind (Just $ p + d) points) cs prev_cs True
+    solveLength (p :: Pos) ind ind0 a_mult len = case findAngleConstr ind0 ind cs of
       Nothing -> skipConstr
-      Just angle -> solveConstraints (M.insert ind points, Just $ p + mkPolar len angle * a_mult) (deleteAngleConstr ind0 ind cs) prev_cs True
+      Just angle -> solveConstraints (M.insert ind (Just $ p + mkPolar len (angle * a_mult)) points)
+                                     (deleteAngleConstr ind0 ind cs) prev_cs True
 
-checkSolution points constrs | any isNothing $ M.elems points = error "couldn't find a complete solution, got " ++ show M.elems points
-checkSolution points cs = let ps = M.map fromJust points
-                          in checkConstrs ps cs
+checkEps :: RealT -> a -> String -> a
+checkEps val cont msg = if abs val < eps then cont else error $ msg
 
-checkConstrs points = M.elems points
-checkConstrs points (c:cs) = case c of
-  Disp ind1 ind2 d ->  check $ abs $ points ! ind2 - points ! ind1  - d
-  Length ind1 ind2 l -> check $ abs  (points ! ind2 - points ! ind1) - l
-  Angle ind1 ind2 a 0 -> check (phase (points ! ind2 - points ! ind1) - a)
+calcDiscrepancy :: (UniConstraint Pos) -> RealT
+calcDiscrepancy c =
+  case c of
+    Disp p1 p2 d -> magnitude $ p2 - p1 - d
+    Length p1 p2 l -> magnitude (p2 - p1) - l
+    Angle p1 p2 a -> phase (p2 - p1) - a
+
+checkSolution points constrs | any isNothing $ M.elems points = error $ "couldn't find a complete solution, got " ++ show (M.elems points)
+checkSolution points cs = checkConstrs points cs
+
+checkConstrs :: FigMap -> [UniConstraint PointInd] -> FigMap
+checkConstrs points [] = points
+checkConstrs points (c:cs) = check $ calcDiscrepancy $ fmap (fromJust . (points !)) c
  where p1 = cPoint1 c
        p2 = cPoint2 c
-       check x = if abs x < eps then checkConstrs points cs
-                 else error $ printf "error checking constraint %s on points %d %d placed at %s %s"
-                      (show c) p1 p2 (show $ points ! p1) (show $ points ! p2)
+       check x = checkEps x (checkConstrs points cs) $
+                   printf "error checking constraint %s on points %s %s placed at %s %s"
+                   (show c) (show p1) (show p2) (show $ points ! p1) (show $ points ! p2)
 
--- finds first angle constraint on points with speified indices
+-- finds first angle constraint on points with specified indices
+findAngleConstr :: PointInd -> PointInd -> [Constraint] -> Maybe RealT
 findAngleConstr _ _ [] = Nothing
 findAngleConstr ind1 ind2 (c:cs) = case c of
   Angle i1 i2 a | i1 == ind1 && i2 == ind2 -> Just a
@@ -111,5 +131,5 @@ findAngleConstr ind1 ind2 (c:cs) = case c of
   _ -> findAngleConstr ind1 ind2 cs
 
 -- deletes first angle constraint
-deleteAngleConstr ind1 ind2 = deleteBy (\_ (Angle i1 Angle i2) -> inds == (i1, i2) || inds == (i2, i1)) undefined
-  where inds = (ind1,ind2)
+deleteAngleConstr ind1 ind2 = deleteBy (\(Angle d1 d2 _) (Angle i1 i2 _) -> (i1, i2) == (d2,d1) || (i2, i1) == (d1,d2))
+                              (Angle ind1 ind2 undefined)
